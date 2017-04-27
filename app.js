9c979773aab6c7398810a699dd5327a35fd89bd9
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-04-27 14:27:05 -0400

    Allow remote path

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const app = require('express')();
 const debug = require('debug')('DummyProxy:app');
 const proxy = require('express-http-proxy');
+const url = require('url');
 
 const config = require('./config');
 
 function addConfig(app, pathConfig) {
     debug(`Adding ${pathConfig.path} => ${pathConfig.remote}`);
-    app.use(pathConfig.path, proxy(pathConfig.remote));
+
+    const proxyPath = url.parse(pathConfig.remote).path;
+
+    app.use(pathConfig.path, proxy(pathConfig.remote, {
+        proxyReqPathResolver(req) {
+            const reqPath = url.parse(req.url).path;
+            return [proxyPath, reqPath].join('/');
+        }
+    }));
 }
 
 app.get('/', (req, res) => {
