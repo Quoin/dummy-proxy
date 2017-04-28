9cde6b8221c02c6a2ded3a0696012fbc5aa5aa24
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-04-28 11:38:23 -0400

    Added 'X-Forwarded-*' and 'Forwarded' headers

---
+++ RELEASES.md
@@ -0,0 +1,5 @@
+# Releases
+
+## 0.1.1 - 2017-04-28
+
+- Added `X-Forwarded-*` and `Forwarded` headers.
--- app.js
+++ app.js
@@ -12,12 +12,63 @@ function addConfig(app, pathConfig) {
 
     app.use(pathConfig.path, proxy(pathConfig.remote, {
         proxyReqPathResolver(req) {
-            const reqPath = url.parse(req.url).path;
+            let reqPath = url.parse(req.url).path;
+            if (reqPath === '/') {
+                return proxyPath;
+            }
+            debug(`proxyReqPathResolver(): proxyPath=${proxyPath}; reqPath=${reqPath}`);
             return [proxyPath, reqPath].join('/');
+        },
+
+        proxyReqOptDecorator(proxyReqOpts, srcReq) {
+            const headers = proxyReqOpts.headers;
+
+            // X-Forwarded-Path
+            //      We want to keep the original because that's what the browser
+            //      will use.
+            if (!headers['x-forwarded-path']) {
+                headers['x-forwarded-path'] = pathConfig.path;
+            }
+
+            // X-Forwarded-For
+            let remoteAddress = srcReq.connection.remoteAddress;
+            if (remoteAddress.match(/:/)) {
+                remoteAddress = `[${remoteAddress}]`;
+            }
+            if (headers['x-forwarded-for']) {
+                headers['x-forwarded-for'] = headers['x-forwarded-for'].split(/,\s*/).concat(remoteAddress).join(', ');
+            } else {
+                headers['x-forwarded-for'] = remoteAddress;
+            }
+
+            // X-Forwarded-Host
+            if (!headers['x-forwarded-host']) {
+                headers['x-forwarded-host'] = srcReq.headers.host;
+            }
+
+            // X-Forwarded-Proto
+            if (!headers['x-forwarded-proto']) {
+                headers['x-forwarded-proto'] = srcReq.protocol;
+            }
+
+            // Forwarded
+            if (!headers['forwarded']) {
+                headers['forwarded'] = [
+                    `path=${headers['x-forwarded-path']}`,
+                    `for=${headers['x-forwarded-for']}`,
+                    `host=${headers['x-forwarded-host']}`,
+                    `proto=${headers['x-forwarded-proto']}`
+                ].join('; ');
+            } else {
+                headers['forwarded'] = headers['forwarded'].split(/;\s*/).concat([`for=${remoteAddress}`]).join('; ');
+            }
+
+            return proxyReqOpts;
         }
     }));
 }
 
+app.enable('trust proxy');
 app.get('/', (req, res) => {
     res.send(
         ['<ul>'].concat(
@@ -27,6 +78,11 @@ app.get('/', (req, res) => {
         ).concat(['</ul>']).join('')
     );
 });
+
+app.get('/_foo', (req, res) => {
+    res.status(200).send(req.headers);
+});
+
 config.paths.forEach(addConfig.bind(null, app));
 
 module.exports = app;
